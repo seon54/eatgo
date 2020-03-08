454bf5061500b03b2fe8150fba37df2d8f09b405
seon54 <seonhyeongpark54@gmail.com>
2020-03-08 23:30:48 +0900

    Chap 1 - 12

---
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItem.java
@@ -0,0 +1,14 @@
+package kr.co.fastcampus.eatgo.domain;
+
+public class MenuItem {
+    private final String menu;
+
+    public MenuItem(String menu) {
+        this.menu = menu;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+}
---
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -0,0 +1,47 @@
+package kr.co.fastcampus.eatgo.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Restaurant {
+    private final String name;
+    private final String address;
+    private final Long id;
+    private List<MenuItem> menuItems = new ArrayList<MenuItem>();
+
+    public Restaurant(Long id, String name, String address) {
+        this.name = name;
+        this.address = address;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInformation() {
+        return name + " in " + address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public void addMenuItem(MenuItem menu) {
+        menuItems.add(menu);
+    }
+
+    public void setMenuItems(List<MenuItem> menuItems) {
+        for (MenuItem menuItem : menuItems) {
+            addMenuItem(menuItem);
+        }
+    }
+}
