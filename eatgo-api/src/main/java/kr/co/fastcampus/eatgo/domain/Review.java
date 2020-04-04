f6d5bf148939bc7ad5da02e28438d2fa4164f8d3
seon54 <seonhyeongpark54@gmail.com>
2020-04-04 21:03:32 +0900

    Update chap.27

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Review.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/Review.java
@@ -2,14 +2,19 @@ package kr.co.fastcampus.eatgo.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Builder
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Review {
@@ -17,4 +22,14 @@ public class Review {
     @Id
     @GeneratedValue
     private Long id;
+
+    @NotEmpty
+    private String name;
+
+    @Min(0)
+    @Max(5)
+    private Integer score;
+
+    @NotEmpty
+    private String description;
 }
