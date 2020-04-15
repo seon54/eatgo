1ed26c7ca1b9fce637fc06b73e3ea76e07cad253
seon54 <seonhyeongpark54@gmail.com>
2020-04-15 19:02:06 +0900

    Update chap.41-42

---
+++ eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/application/UserService.java
@@ -0,0 +1,29 @@
+package kr.co.fastcampus.eatgo.application;
+
+import kr.co.fastcampus.eatgo.domain.User;
+import kr.co.fastcampus.eatgo.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User addUser(String email, String name) {
+        return null;
+    }
+}
---
+++ eatgo-admin-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/UserControllerTests.java
@@ -0,0 +1,65 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import kr.co.fastcampus.eatgo.application.UserService;
+import kr.co.fastcampus.eatgo.domain.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(UserController.class)
+class UserControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    public void list() throws Exception {
+        List<User> users = new ArrayList<>();
+
+        users.add(User.builder().email("tester@example.com").name("tester").level(1L).build());
+
+        given(userService.getUsers()).willReturn(users);
+
+        mvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("tester")));
+    }
+
+    @Test
+    public void create() throws Exception {
+        String email = "admin@example.com";
+        String name = "admin";
+
+        User user = User.builder().email(email).name(name).build();
+
+        given(userService.addUser(email, name)).willReturn(user);
+
+        mvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"email\":\"admin@example.com\",\"name\":\"admin\"}"))
+                .andExpect(status().isCreated());
+
+        verify(userService).addUser(email, name);
+    }
+
+}
